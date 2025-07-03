3b3b2925155b6bef2edf17c73b644c5ed11ee9c1
moir_shota <mori.shota.q@alhinc.jp>
2025-07-03 16:32:29 +0900

    [Add] ユーザー編集画面・機能追加

--- src/main/java/com/example/Attendance/management/controller/form/AttendanceForm.java
+++ src/main/java/com/example/Attendance/management/controller/form/AttendanceForm.java
@@ -24,13 +24,13 @@ public class AttendanceForm {
 
     @NotBlank(message = "出勤時間を入力してください")
     @Pattern(regexp = "^[^　]*$", message = "出勤時間を入力してください")
-    @Pattern(regexp = "^([01]\\d|1[0-3]):[0-5]\\d$", message = "半角英数字で入力してください")
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "半角英数字で入力してください")
     //@Size(max = 5, message = "アカウントは6文字以上20文字以内で入力してください")
     private String attendance;
 
     @NotBlank(message = "出勤時間を入力してください")
     @Pattern(regexp = "^[^　]*$", message = "出勤時間を入力してください")
-    @Pattern(regexp = "^([01]\\d|1[0-3]):[0-5]\\d$", message = "半角英数字で入力してください")
+    @Pattern(regexp = "^([01]\\d|2[0-3]):[0-5]\\d$", message = "半角英数字で入力してください")
     private String leave;
 
 
