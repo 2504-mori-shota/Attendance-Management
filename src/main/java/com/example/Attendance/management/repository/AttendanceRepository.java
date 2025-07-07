18a36ff771dcfb6996c3415d9acc68e090b32e00
moir_shota <mori.shota.q@alhinc.jp>
2025-07-07 15:22:26 +0900

    [Update] 日付を被って登録不可追加

--- src/main/java/com/example/Attendance/management/repository/AttendanceRepository.java
+++ src/main/java/com/example/Attendance/management/repository/AttendanceRepository.java
@@ -2,6 +2,8 @@ package com.example.Attendance.management.repository;
 
 import com.example.Attendance.management.repository.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -16,5 +18,11 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     List<Attendance> findByUserIdAndAttendanceBetweenOrderByAttendanceAsc(int userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
 
     List<Attendance> findById(int id);
+    //"yyyy-MM"で勤怠情報取得
+    @Query("SELECT a FROM Attendance a WHERE a.userId = :userId AND a.attendance >= :start AND a.attendance <= :end")
+    List<Attendance> findByUserIdAndAttendanceBetween(@Param("userId")int userId, @Param("start")LocalDateTime start, @Param("end") LocalDateTime end);
+    //"HH:mm"で勤怠時間取得
+    @Query("SELECT a FROM Attendance a WHERE a.userId = :userId AND a.attendance > :start AND a.leave < :end")
+    List<Attendance> findByUserIdAndAttendanceANDLeaveBetween(@Param("userId")int userId, @Param("start")LocalDateTime start, @Param("end") LocalDateTime end);
 
 }
