0c1375fd53f52d5eee02dd0e9906a42cd9daebc7
moir_shota <mori.shota.q@alhinc.jp>
2025-07-08 14:28:10 +0900

    [Update] プル用

--- src/main/java/com/example/Attendance/management/repository/RequestRepository.java
+++ src/main/java/com/example/Attendance/management/repository/RequestRepository.java
@@ -1,13 +1,20 @@
 package com.example.Attendance.management.repository;
 
+import com.example.Attendance.management.repository.entity.Attendance;
 import com.example.Attendance.management.repository.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Integer> {
     List<Request> findAllByOrderByCreatedDate();
+
     List<Request> findById(int id);
+
 }
